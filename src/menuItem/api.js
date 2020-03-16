2759f2715bd76ad4545e0984712c8b99637dfd06
aLi <aaljamid@emich.edu>
2020-03-16 14:49:12 +0300

    add axios 'add' to the api.js

--- src/menuItem/api.js
+++ src/menuItem/api.js
@@ -1,8 +1,15 @@
-// Import the API configration and axios
+// Import the API configuration and axios
 import apiUrl from "../apiConfig";
 import axios from "axios";
 
 //Get All menu items
 export const getAllMenuItems = () => {
+  // return a promise
   return axios.get(`${apiUrl}/api/menu-items`);
+};
+
+// Add Menu Item
+export const addMenuItem = menuItem => {
+  // return a promise
+  return axios.post(`${apiUrl}/api/menu-items`, { menuItem });
 };
