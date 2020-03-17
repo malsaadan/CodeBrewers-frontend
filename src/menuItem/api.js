2339fd57e3d4ce3432b5c5fda849d31871c20a45
malsaadan <mmalsaadan@gmail.com>
2020-03-17 10:15:00 +0300

    Add edit menu item using API

--- src/menuItem/api.js
+++ src/menuItem/api.js
@@ -1,8 +1,21 @@
-// Import the API configration and axios
+// Import the API configuration and axios
 import apiUrl from "../apiConfig";
 import axios from "axios";
 
 //Get All menu items
 export const getAllMenuItems = () => {
   return axios.get(`${apiUrl}/api/menu-items`);
+};
+
+// Delete MenuItem By ID
+export const deleteMenuItemByID = id => {
+  // return a promise so that I can catch the error & display it in the UI
+  return axios.delete(`${apiUrl}/api/menu-items/${id}`);
+};
+
+// Edit MenuItem by ID
+export const editMenuItemByID = (id, menuItem) => {
+  // return a promise so that I can catch the error & display it in the UI
+  // Add the id in the params to edit a specific article and pass the new article as object
+  return axios.patch(`${apiUrl}/api/menu-items/${id}`, { menuItem });
 };
