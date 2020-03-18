6f0f20ebda6212b6a28a7d6accdec706b957e915
malsaadan <mmalsaadan@gmail.com>
2020-03-18 16:20:33 +0300

    Updates

---
+++ precache-manifest.617a1e9378c4d30bf1ae8159d1f6d9ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e9560b50bed1184c118c8e2001fe1d",
+    "url": "/CodeBrewers-frontend/index.html"
+  },
+  {
+    "revision": "c7a61d0a1066e5cd1e9f",
+    "url": "/CodeBrewers-frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "92841bf6f260336cdd0b",
+    "url": "/CodeBrewers-frontend/static/css/main.b6f9abe6.chunk.css"
+  },
+  {
+    "revision": "c7a61d0a1066e5cd1e9f",
+    "url": "/CodeBrewers-frontend/static/js/2.6e43f01f.chunk.js"
+  },
+  {
+    "revision": "d7b319d8a6f4d5773bf6cc977c7731d1",
+    "url": "/CodeBrewers-frontend/static/js/2.6e43f01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92841bf6f260336cdd0b",
+    "url": "/CodeBrewers-frontend/static/js/main.40a876d9.chunk.js"
+  },
+  {
+    "revision": "2aa6989eb18987bd464b",
+    "url": "/CodeBrewers-frontend/static/js/runtime-main.37c02413.js"
+  },
+  {
+    "revision": "5867d789bbc6337eee356496cd78e73c",
+    "url": "/CodeBrewers-frontend/static/media/coffee.5867d789.png"
+  }
+]);
